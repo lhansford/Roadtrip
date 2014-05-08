22f0e8ed1a48f986a2b29b3c23999a0147fd08a6
Luke Hansford <l.s.hansford@gmail.com>
2014-05-09 07:28:26 +1000

    Added method skeleton

--- roadtrip/views.py
+++ roadtrip/views.py
@@ -5,3 +5,29 @@ from roadtrip import app
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/newtrip')
+def new_trip():
+	""" Create a new road trip."""
+	pass
+
+@app.route('/trip/<trip_name>')
+def trip(trip_name):
+	""" Loads a particular trip """
+	pass
+
+@app.route('/edit/<trip_name>')
+def edit_trip(trip_name):
+	""" Edit a given road trip. """
+	pass
+
+def upload_image(trip, day=None):
+	""" Uploads image(s) to a roadtrip. A day might be specified, but if not the
+	photos are added to an unused bin.
+	"""
+	pass
+
+def delete_image(image):
+	""" Delete a particular image"""
+	pass
+
